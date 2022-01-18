e2a3fe65feb75c737343803f4f02595fd78e3a4d
siddhesh <siddhesh.andhari@gmail.com>
2022-01-17 23:17:32 -0500

    testing libraries added

--- app/src/androidTest/java/com/example/filmenthusiasts/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/filmenthusiasts/ExampleInstrumentedTest.java
@@ -2,12 +2,21 @@ package com.example.filmenthusiasts;
 
 import android.content.Context;
 
+import com.example.filmenthusiasts.activities.main_activity.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -22,5 +31,16 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.filmenthusiasts", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testView() {
+        onView(withId(R.id.main_activity_recyclerview))
+                .perform(click())
+                .check(matches(isDisplayed()));
     }
 }
